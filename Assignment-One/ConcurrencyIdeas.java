59532b8f590c8acb03f1f432c9b0643c57446282
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-02-17 18:50:29 +0000

    Update CocurrencyIdeas.java

--- Assignment-One/ConcurrencyIdeas.java
+++ Assignment-One/ConcurrencyIdeas.java
@@ -1,19 +1,24 @@
 //to look at concurrent classes to see what is available, atomic units etc. Graphics?
 
 
-
 class elev
 {
+	// Shane: Type string for clarity? but we could do boolean.
 	String/enum direction = up/down;
 	int currentFloor = 0;
 	final int maxWeightCapacity = XXX;
 	boolean isFull;
+
+	// Shane: Should this be LIFO? Is the last person in always the first one off?
+	// This would only make sense if everyone got off at the same floor.
 	Person Arraylist currentPassengers = []; 
-	//cant really use stack as its not necessarily LIFO, but if we are to model over capacity we need to remember the last person in
+	// Cant really use stack as its not necessarily LIFO, but if we are to model over capacity we need to remember the last person in
 	
-	concurrentQueue requests;
+	// Shane: BlockingQueue here potentially
+	ConcurrentQueue requests;	
+
 	//or make requests class?
-	int elevatorID //if multiple for marks
+	int elevatorID // if multiple for marks
 	boolean outOfOrder;
 
 }
@@ -21,11 +26,14 @@ class elev
 class person
 {
 	String id
+	// Shane: Initially have standard weight but allow for variation later.
 	int Weight??? //might just make standard
 	Luggage LuggageID
 	int arrivalTime
 	int arrivalFloor
 	int destFloor
+
+	// Shane: Yeah, I think it is a good idea to have these RNG sort of lower probability situations.
 	boolean pickedCorrectButton; //do mathRandom, could be tricky to do without locking
 }
 
