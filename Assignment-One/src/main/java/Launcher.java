6fccfd2ad02d0d93259132632ec074cf4760b75a
kinses38 <shaun.kinsella38@mail.dcu.ie2>
2019-03-19 14:07:04 +0000

    Add loops for await

--- Assignment-One/src/main/java/Launcher.java
+++ Assignment-One/src/main/java/Launcher.java
@@ -4,7 +4,6 @@ public class Launcher {
 
     public static void main(String[] args) {
         Airport airport = new Airport(3, 1);
-
         // If GUI parameter passed, start gui
         if(args.length > 0 && args[0].toLowerCase().equals("--gui")) {
             new Thread(() -> airport.initialize()).start();
